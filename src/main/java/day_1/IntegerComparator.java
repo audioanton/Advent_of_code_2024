043c8ab805419c235d2418e9f7eb1ab8f86330ed
Anton Larsson <antonlarsson.musik@gmail.com>
2024-12-01 16:34:19 +0100

    Day 1 part one passed.

---
+++ src/main/java/day_1/IntegerComparator.java
@@ -0,0 +1,52 @@
+package day_1;
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class IntegerComparator {
+    int leftColumn;
+    int rightColumn;
+    int result;
+    List<Integer> leftColumnList;
+    List<Integer> rightColumnList;
+
+    public IntegerComparator() {
+        leftColumn = 0;
+        rightColumn = 0;
+        result = 0;
+        leftColumnList = new ArrayList<>();
+        rightColumnList = new ArrayList<>();
+    }
+
+    public int getSumOfDifferenceInString(String s) {
+        try (Scanner scanner = new Scanner(s)) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                separateIntPerColumn(line);
+            }
+
+            Collections.sort(leftColumnList);
+            Collections.sort(rightColumnList);
+
+            for (int i = 0; i < leftColumnList.size(); i++) {
+                result += compareTwoIntegers(leftColumnList.get(i), rightColumnList.get(i));
+            }
+        }
+        return result;
+    }
+
+    public void separateIntPerColumn(String s) {
+        String[] twoStrings = s.split("   ");
+        leftColumnList.add(Integer.parseInt(twoStrings[0]));
+        rightColumnList.add(Integer.parseInt(twoStrings[1]));
+    }
+
+    public int compareTwoIntegers(int firstInt, int secondInt) {
+        int max = Math.max(firstInt, secondInt);
+        int min = Math.min(firstInt, secondInt);
+        return max - min;
+    }
+}
---
+++ src/test/java/day_1/IntegerComparatorTest.java
@@ -0,0 +1,49 @@
+package day_1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+class IntegerComparatorTest {
+    IntegerComparator ic = new IntegerComparator();
+    String fullInput =
+            "3   4\n" +
+            "4   3\n" +
+            "2   5\n" +
+            "1   3\n" +
+            "3   9\n" +
+            "3   3";
+
+    String firstRow = "3   4";
+    String secondRow = "4   3";
+
+    @Test
+    void getSumOfDifferenceInStringTest() {
+        int result = ic.getSumOfDifferenceInString(fullInput);
+        assertNotEquals(0, result);
+        assertEquals(11, result);
+    }
+
+    @Test
+    void separateIntPerColumnTest() {
+        ic.separateIntPerColumn(firstRow);
+        assertEquals(3, ic.leftColumnList.getFirst());
+        assertEquals(4, ic.rightColumnList.getFirst());
+
+        ic.separateIntPerColumn(secondRow);
+        assertNotEquals(3, ic.leftColumnList.getLast());
+        assertEquals(4, ic.leftColumnList.getLast());
+        assertEquals(3, ic.rightColumnList.getLast());
+    }
+
+    @Test
+    void compareTwoIntegersTest() {
+        assertNotEquals(0, ic.compareTwoIntegers(1, 2));
+        assertNotEquals(0, ic.compareTwoIntegers(2, 1));
+        assertEquals(0, ic.compareTwoIntegers(3, 3));
+        assertEquals(1, ic.compareTwoIntegers(4, 5));
+        assertEquals(6, ic.compareTwoIntegers(3, 9));
+        assertEquals(6, ic.compareTwoIntegers(9, 3));
+    }
+}
